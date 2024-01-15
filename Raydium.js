69aec04f00c21b791d20b01fa4900c8a864a2fad
rain <rainnier@gmail.com>
2024-01-16 04:05:26 +0800

    update accepts object as parameter send

--- Raydium.js
+++ Raydium.js
@@ -21,84 +21,89 @@ class Raydium {
     slippagePercent = 10,
     txnMode = 'BUY',
   }) => {
-    const getTokenAccounts = async () => {
-      const tokenResp = await this.connection.getTokenAccountsByOwner(
-        this.wallet.publicKey,
-        {
-          programId: TOKEN_PROGRAM_ID,
+    try {
+      const getTokenAccounts = async () => {
+        const tokenResp = await this.connection.getTokenAccountsByOwner(
+          this.wallet.publicKey,
+          {
+            programId: TOKEN_PROGRAM_ID,
+          }
+        )
+
+        const accounts = []
+        for (const { pubkey, account } of tokenResp.value) {
+          accounts.push({
+            pubkey,
+            accountInfo: SPL_ACCOUNT_LAYOUT.decode(account.data),
+          })
+        }
+
+        return accounts
+      }
+
+      const tokenAccounts = await getTokenAccounts()
+      console.log('starting pool keys')
+      const poolKeys = await PoolKey.getPoolKeys(newCoinAddress, outMintAddress)
+      console.log(
+        poolKeys
+          ? 'Successful retrieval of poolkeys'
+          : `Cannot get poolkeys for ${newCoinAddress}`
+      )
+      if (!poolKeys) {
+        return {
+          status: 'FAIL_NO_POOL_KEYS',
+          txn: `No poolkeys for ${newCoinAddress}`,
+        }
+      }
+      console.log('end pool keys')
+
+      // Fetch the pool information
+      const slippage = new Percent(slippagePercent, 100)
+      const { amountInRaw, amountOutRaw } =
+        await AmtChecker.computeMinimumAmountWithSlippage({
+          connection: this.connection,
+          poolKeys,
+          inMintToSwap: solToSwap,
+          slippage,
+          txnMode,
+        })
+      const swapInstruction = await Liquidity.makeSwapInstructionSimple({
+        connection: this.connection,
+        poolKeys,
+        userKeys: {
+          tokenAccounts,
+          owner: this.wallet.publicKey,
+        },
+        amountIn: amountInRaw,
+        amountOut: amountOutRaw,
+        fixedSide: 'in',
+      })
+
+      // Send the transaction
+      const txn = new Transaction()
+      swapInstruction.innerTransactions[0].instructions.forEach(
+        (instruction) => {
+          txn.add(instruction)
         }
       )
+      const result = await this.connection.getLatestBlockhash()
+      txn.recentBlockhash = result.blockhash
+      txn.feePayer = this.wallet.publicKey
+      txn.partialSign(this.wallet.payer)
 
-      const accounts = []
-      for (const { pubkey, account } of tokenResp.value) {
-        accounts.push({
-          pubkey,
-          accountInfo: SPL_ACCOUNT_LAYOUT.decode(account.data),
-        })
+      let txid = await this.connection.sendRawTransaction(txn.serialize())
+      console.log('Transaction:', `https://solscan.io/tx/${txid}`)
+
+      try {
+        await this.connection.confirmTransaction(txid)
+        console.log(`Success Raydium swap https://solscan.io/tx/${txid}`)
+        return { status: 'SUCCESS', txn: `https://solscan.io/tx/${txid}` }
+      } catch (error) {
+        console.log(`Fail Raydium swap https://solscan.io/tx/${txid}`)
+        return { status: 'FAIL', txn: `https://solscan.io/tx/${txid}` }
       }
-
-      return accounts
-    }
-
-    const tokenAccounts = await getTokenAccounts()
-    console.log('starting pool keys')
-    const poolKeys = await PoolKey.getPoolKeys(newCoinAddress, outMintAddress)
-    console.log(
-      poolKeys
-        ? 'Successful retrieval of poolkeys'
-        : `Cannot get poolkeys for ${newCoinAddress}`
-    )
-    if (!poolKeys) {
-      return {
-        status: 'FAIL_NO_POOL_KEYS',
-        txn: `No poolkeys for ${newCoinAddress}`,
-      }
-    }
-    console.log('end pool keys')
-
-    // Fetch the pool information
-    const slippage = new Percent(slippagePercent, 100)
-    const { amountInRaw, amountOutRaw } =
-      await AmtChecker.computeMinimumAmountWithSlippage(
-        this.connection,
-        poolKeys,
-        solToSwap,
-        slippage,
-        txnMode
-      )
-    //   console.log(poolKeys)
-    const swapInstruction = await Liquidity.makeSwapInstructionSimple({
-      connection: this.connection,
-      poolKeys,
-      userKeys: {
-        tokenAccounts,
-        owner: this.wallet.publicKey,
-      },
-      amountIn: amountInRaw,
-      amountOut: amountOutRaw,
-      fixedSide: 'in',
-    })
-
-    // Send the transaction
-    const txn = new Transaction()
-    swapInstruction.innerTransactions[0].instructions.forEach((instruction) => {
-      txn.add(instruction)
-    })
-    const result = await this.connection.getLatestBlockhash()
-    txn.recentBlockhash = result.blockhash
-    txn.feePayer = this.wallet.publicKey
-    txn.partialSign(this.wallet.payer)
-
-    let txid = await this.connection.sendRawTransaction(txn.serialize())
-    console.log('Transaction:', `https://solscan.io/tx/${txid}`)
-
-    try {
-      await this.connection.confirmTransaction(txid)
-      console.log(`Success Raydium swap https://solscan.io/tx/${txid}`)
-      return { status: 'SUCCESS', txn: `https://solscan.io/tx/${txid}` }
     } catch (error) {
-      console.log(`Fail Raydium swap https://solscan.io/tx/${txid}`)
-      return { status: 'FAIL', txn: `https://solscan.io/tx/${txid}` }
+      console.log(error)
     }
   }
 }
